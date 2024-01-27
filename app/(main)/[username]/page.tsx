96d16381c2106a6a65b951f780addf8751cf7b6c
Tyler Marshall <tylerjeremiahh@gmail.com>
2024-01-27 14:24:35 +0000

    Quick tweaks

--- app/(main)/[username]/page.tsx
+++ app/(main)/[username]/page.tsx
@@ -19,7 +19,7 @@ export default function Page({ params }: { params: { username: string } }) {
         <div>
             {/* User's profile */}
             <div className="text-white flex flex-row justify-between items-start border-white border-solid border-0 border-b pb-4 mb-4">
-                <Card className="p-4 mx-2 max-w-[400px]">
+                <Card className="p-4 mr-2 max-w-[400px]">
                     <div className="pb-4 border-gray-200 border-solid border-0 border-b-2">
                         <span className="text-lg font-bold">{params.username}</span>
                     </div>
@@ -38,7 +38,7 @@ export default function Page({ params }: { params: { username: string } }) {
                         <span className="text-sm">Following</span>
                     </Card>
                 </div>
-                <div className="flex flex-col justify-center items-center mx-2">
+                <div className="flex flex-col justify-center items-center ml-2">
                     <div className="mb-4 flex flex-row justify-center items-center">
                         <div className="bg-sky-700 h-[80px] w-[80px] rounded-full flex flex-row justify-center items-center">
                             <span className="font-bold text-white" style={{ fontSize: 25 }}>
@@ -47,7 +47,7 @@ export default function Page({ params }: { params: { username: string } }) {
                         </div>
                     </div>
 
-                    <Button variant="outlined" size="small" className="mx-4 min-w-[100px] border-white text-white">
+                    <Button variant="outlined" size="small" className="min-w-[100px] border-white text-white">
                         Follow
                     </Button>
                 </div>
